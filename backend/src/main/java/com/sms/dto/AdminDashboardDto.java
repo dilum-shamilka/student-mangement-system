155587b2047bb61d5bc6bf332ab81e66358c6861
Dilum Shamilka Pahasara <dilumshamilka4@gmail.com>
2026-08-03 11:21:24 +0530

    refactor-admin dashboard content

---
+++ backend/src/main/java/com/sms/dto/AdminDashboardDto.java
@@ -0,0 +1,83 @@
+package com.sms.dto;
+
+
+public class AdminDashboardDto {
+
+
+    private long totalStudents;
+
+    private long totalCourses;
+
+    private long activeStudents;
+
+    private long totalUsers;
+
+
+
+    public AdminDashboardDto(){}
+
+
+
+    public AdminDashboardDto(
+            long totalStudents,
+            long totalCourses,
+            long activeStudents,
+            long totalUsers
+    ){
+
+        this.totalStudents = totalStudents;
+        this.totalCourses = totalCourses;
+        this.activeStudents = activeStudents;
+        this.totalUsers = totalUsers;
+
+    }
+
+
+
+    public long getTotalStudents(){
+        return totalStudents;
+    }
+
+
+    public void setTotalStudents(long totalStudents){
+        this.totalStudents = totalStudents;
+    }
+
+
+
+
+    public long getTotalCourses(){
+        return totalCourses;
+    }
+
+
+    public void setTotalCourses(long totalCourses){
+        this.totalCourses = totalCourses;
+    }
+
+
+
+
+    public long getActiveStudents(){
+        return activeStudents;
+    }
+
+
+    public void setActiveStudents(long activeStudents){
+        this.activeStudents = activeStudents;
+    }
+
+
+
+
+    public long getTotalUsers(){
+        return totalUsers;
+    }
+
+
+    public void setTotalUsers(long totalUsers){
+        this.totalUsers = totalUsers;
+    }
+
+
+}
